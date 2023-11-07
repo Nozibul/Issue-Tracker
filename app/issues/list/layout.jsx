ae40d38d4fd0168469a5b8e370331796b2c93d59
Nozibul <nozibulislamspi@gmail.com>
2023-11-07 12:45:57 +0600

    Show the Issues

---
+++ app/issues/list/layout.jsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+    title: "Issue list",
+    description: "Issue Tracker List of Item",
+  };
+
+const IssueLayout = ({children}) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default IssueLayout  ;
---
+++ app/issues/list/page.jsx
@@ -0,0 +1,53 @@
+import { IssueStatus, Link } from "@/app/components";
+import { Table } from "@radix-ui/themes";
+// import IssueActionBtn from "./IssueActionBtn";
+import prisma from "@/prisma/client";
+
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
+
+  return (
+    <>
+      {/* <IssueActionBtn /> */}
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {issues?.length > 0
+            ? issues.map((issue) => (
+                <Table.Row key={issue.id}>
+                  <Table.Cell>
+                    <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                    <div className="block md:hidden">
+                      <IssueStatus status={issue.status} />
+                    </div>
+                  </Table.Cell>
+                  <Table.Cell className="hidden md:table-cell">
+                    <IssueStatus status={issue.status} />
+                  </Table.Cell>
+                  <Table.Cell className="hidden md:table-cell">
+                    {issue.createdAt.toDateString()}
+                  </Table.Cell>
+                </Table.Row>
+              ))
+            : null}
+        </Table.Body>
+      </Table.Root>
+    </>
+  );
+};
+
+// export const revalidate = 60;
+export const dynamic = "force-dynamic";
+
+export default IssuesPage;
