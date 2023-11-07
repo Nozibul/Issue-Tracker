15869edfee6c1bf768a1618e7bc5483c72b289ba
Nozibul <nozibulislamspi@gmail.com>
2023-11-07 11:45:26 +0600

    Create ErrorMessage component

---
+++ app/components/ErrorMessage.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Text } from "@radix-ui/themes";
+
+const ErrorMessage = ({children}) => {
+    if(!children) return null ;
+  return (
+    <Text color="red" as="p">{children}</Text>
+  )
+}
+
+export default ErrorMessage;
