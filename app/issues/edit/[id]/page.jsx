e9126d5fc177f1d5540e12d048d22e374b76004d
Nozibul <nozibulislamspi@gmail.com>
2023-11-08 10:36:52 +0600

    Build the Edit Issue

---
+++ app/issues/edit/[id]/page.jsx
@@ -0,0 +1,26 @@
+
+import dynamic from "next/dynamic";
+import prisma from "../../../../../prisma/client";
+import { notFound } from "next/navigation";
+import IssueFormLoading from "./loading";
+
+const IssueForm = dynamic(
+  ()=> import('@/app/issues/components/IssueForm'),
+  {
+    ssr: false,
+    loading: ()=> <IssueFormLoading />
+  }
+);
+
+const EditIssuePage = async ({ params }) => {
+  
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) return notFound();
+
+  return <div> <IssueForm issue={issue} /> </div>;
+};
+
+export default EditIssuePage;
